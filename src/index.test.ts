b408a89975ed05bc80b669bad6bdf00ea16a6858
Pedro La Rosa <pedrolarosad@gmail.com>
2019-07-30 18:02:23 +0200

    first commit

---
+++ src/index.test.ts
@@ -0,0 +1,7 @@
+import myName from "./index";
+
+describe("Dummy first test", () => {
+  it("whats my name?", () => {
+    expect(myName).toBe("pedro la rosa");
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export default "pedro la rosa";
